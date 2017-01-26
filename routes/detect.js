85759bbc1ba9a37228662477e2e411f943a043a2
Chuck Choukalos <cchoukalos@AUS-LM-000427.local>
2017-01-25 20:53:55 -0600

    added batch geoip and draft batch url detect; still needs work on path seperation

--- routes/detect.js
+++ routes/detect.js
@@ -9,15 +9,32 @@ var detectcode  = require('../lib/detect');
 router.post('/url',function(req,res,next) {
    detectcode.UrlDetect(req.body.hostname,req.body.path,function(err,rc) {
      console.log("Post to Detect Route:");
-     res.end(JSON.stringify(rc));
+//     res.setHeader('Content-Type','application/json');
+     res.end(JSON.stringify(rc),null,3);
    });
 });
 
 router.post('/geoip',function(req,res,next) {
 	detectcode.GeoipDetect(req.body.hostname,function(err,rc) {
 		console.log("Post to Detect Geoip:");
-		res.end(JSON.stringify(rc));
+//		res.setHeader('Content-Type','application/json');
+		res.end(JSON.stringify(rc),null,3);
 	});
 });
 
+router.post('/batch/geoip',function(req,res,next){
+	detectcode.BatchGeoip(req.body.hostnames,function(err,rc) {
+		console.log("Post to Detect Batch Geoip:");
+//		res.setHeader('Content-Type','application/json');
+		res.end(JSON.stringify(rc),null,3);	
+	});
+	
+});
+
+router.post('/batch/url',function(req,res,next){
+	detectcode.BatchUrl(req.body.urls,function(err,rc) {
+		console.log("Post to Detect Batch URLs:");
+		res.end(JSON.stringify(rc),null,3);	
+	});
+})
 module.exports = router;
--- test.sh
+++ test.sh
@@ -13,4 +13,10 @@ echo "--Testing pi.choukalos.org"
 curl -XPOST -H "Content-Type: application/json" -d '{"hostname":"pi.choukalos.org","path":"/"}' http://localhost:3000/detect/url/
 echo "--Testing wordpress.com"
 curl -XPOST -H "Content-Type: application/json" -d '{"hostname":"wordpress.com","path":"/"}' http://localhost:3000/detect/url/
+echo ""
+echo "--Testing Batch GEOIP Lookup"
+curl -XPOST -H "Content-Type: application/json" -d '{"hostnames": ["magento.com","google.com","wordpress.com","raspberrypi.org"] }' http://localhost:3000/detect/batch/geoip/
+echo "--Testing Batch URL Lookup"
+curl -XPOST -H "Content-Type: application/json" -d '{"urls": ["magento.com","google.com","stackoverflow.com/questions"]}' http://localhost:3000/detect/batch/url/
 echo "Test finished"
+
