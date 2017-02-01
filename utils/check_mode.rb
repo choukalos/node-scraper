f36c5c01d47570577359306abed6803ccfeba4ec
Chuck Choukalos <cchoukalos@AUS-LM-000427.local>
2017-01-31 19:55:45 -0600

    fixed check_mode.rb script to quickly gather data for Alan on %usage of Default mode

---
+++ utils/check_mode.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'json'
+
+# Global Variables
+BATCHSIZE = 5
+CURLLOC   = "http://localhost:3000/detect/batch/magentomode/"
+CURLHEAD  = '-H "Content-Type: application/json"' 
+
+# Function
+if ARGV.count < 1
+  puts "Syntax:  check_mode.rb FILENAME "
+  puts "  where FILENAME has 1 hostname per file "
+  exit
+end
+
+filename  = ARGV.shift
+hostnames = IO.readlines(filename).collect {|host| host.chomp! }
+numhosts  = hostnames.size
+iterations= numhosts / BATCHSIZE
+
+(0..iterations).each do |i|
+  start = i*BATCHSIZE
+  stop  = i*BATCHSIZE + (BATCHSIZE-1)
+  dohosts = hostnames[start..stop]
+  #puts "processing #{i} - #{start} to #{stop}"
+  payload = '-d {"hostnames": ' + dohosts.to_s + '}'
+  curlcmd = "curl -s #{CURLHEAD} \'#{payload}\' #{CURLLOC}"
+  #puts "CMD:  #{curlcmd}"
+  rc   = `#{curlcmd}`
+  data = JSON.parse(rc)
+  data.each do |x|
+    puts "#{x["hostname"]},#{x["mode"]}"
+  end
+end
+
+
+
+
+
+
